2b44bbf04b6e7b3d74ad68e081157dbb4ffe9fce
Shih-Po Wang <sibo_wang@ot.olympus.co.jp>
2020-09-11 10:24:05 +0900

    update insert_and_update_ticket.sh

--- insert_and_update_ticket.sh
+++ insert_and_update_ticket.sh
@@ -8,9 +8,9 @@ PASS=iY59RsDn
 export http_proxy=http://proxy.olympus.co.jp:8080
 export https_proxy=http://proxy.olympus.co.jp:8080
 
-#PROJECT_LIST=('CV2K製品' 'IPF-3 OTV製品' 'VE2製品試験' 'ツール' \
-#    '教育用testPJ' 'Sample_Env' 'OTV_SOMED_RTC' 'test_SOMED_RTC' 'sandbox_integ' 'Simulator')
-PROJECT_LIST=('CV2K製品')
+PROJECT_LIST=('CV2K製品' 'IPF-3 OTV製品' 'VE2製品試験' 'ツール' \
+    '教育用testPJ' 'Sample_Env' 'OTV_SOMED_RTC' 'test_SOMED_RTC' 'sandbox_integ' 'Simulator')
+#PROJECT_LIST=('CV2K製品')
 
 for project in "${PROJECT_LIST[@]}"; do
     echo $project
